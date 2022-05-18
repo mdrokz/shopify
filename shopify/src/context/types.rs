d17d9dd70e028fa77329d4e5e47fcbae983c6656
mdrokz <mohammadmunshi@gmail.com>
2022-05-18 14:24:46 +0530

    implement ApiVersion

--- shopify/src/context/types.rs
+++ shopify/src/context/types.rs
@@ -1,3 +1,23 @@
-pub enum API_Version {
-    Apr2022
+/// `July21("2021-07")`
+/// 
+/// `October21("2021-10")`
+/// 
+/// `January22("2022-01")`
+/// 
+/// `April22("2022-04")`
+/// 
+/// `Nightly("unstable")`
+
+pub enum ApiVersion {
+  July21(String),    /// "2021-07",
+  October21(String), // "2021-10",
+  January22(String), // "2022-01",
+  April22(String),   // "2022-04",
+  Nightly(String),   // "unstable"
+}
+
+impl Default for ApiVersion {
+  fn default() -> Self {
+    Self::Nightly(String::from("unstable"))
+  }
 }
