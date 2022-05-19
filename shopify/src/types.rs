b0b006f75febaefdd9c46bd9d43aa44f698ee5c9
mdrokz <mohammadmunshi@gmail.com>
2022-05-19 17:32:01 +0530

    implement helper types for query string

--- shopify/src/types.rs
+++ shopify/src/types.rs
@@ -1,2 +1,31 @@
+use std::{collections::HashMap, fmt::Display};
+
 pub use chrono::{DateTime, Utc};
 pub use serde_json::Value;
+
+#[macro_export]
+macro_rules! map {
+    ($($v: literal => $s: expr),*) => {{
+            let mut h = HashMap::new();
+            $(
+                h.insert($v,$s);
+            )*
+            h
+    }}
+}
+
+pub struct Query(pub String);
+
+impl<V: Display> Into<Query> for HashMap<&str, V> {
+  fn into(self) -> Query {
+    let entries = self.iter();
+
+    let mut query = String::new();
+
+    for (key, value) in entries {
+      query.push_str(&format!("{}={}&", key, value));
+    }
+
+    Query(query)
+  }
+}
