789461c18215911d3554d37543b162b233a82587
mdrokz <mohammadmunshi@gmail.com>
2022-05-20 16:55:22 +0530

    complete validate_auth_callback

--- shopify/src/auth/mod.rs
+++ shopify/src/auth/mod.rs
@@ -1,6 +1,8 @@
 use std::borrow::Cow;
 use std::collections::HashMap;
+use std::str::FromStr;
 
+use chrono::{DateTime, Duration as ChronoDuration, Utc};
 use cookie::time::{Duration, OffsetDateTime};
 use cookie::{Cookie, CookieJar, Key};
 use uuid::Uuid;
@@ -17,7 +19,7 @@ pub use self::types::*;
 const SESSION_KEY: &str = "shopify_app_session";
 
 impl Client {
-  pub async fn beginAuth(
+  pub async fn begin_auth(
     &mut self,
     shop: &str,
     redirect_path: &str,
@@ -25,13 +27,13 @@ impl Client {
   ) -> Result<String> {
     let key = Key::from(self.context.api_secret_key.as_bytes());
 
-    let cookie = Cookie::build(SESSION_KEY, "")
+    let session = Session::new(Uuid::new_v4(), shop, "", true);
+
+    let cookie = Cookie::build(SESSION_KEY, session.id.to_string())
       .secure(true)
       .expires(OffsetDateTime::now_utc().checked_add(Duration::seconds(60000)))
       .same_site(cookie::SameSite::Lax)
       .finish();
-
-    let session = Session::new(Uuid::new_v4(), shop, "", true);
 
     self.context.session.store_session(session);
 
@@ -52,14 +54,24 @@ impl Client {
     ))
   }
 
-  pub async fn validateAuthCallback(
-    &self,
+  pub async fn validate_auth_callback(
+    &mut self,
     auth_query: AuthQuery,
-    cookie_jar: &CookieJar,
-  ) -> Result<()> {
+    cookie_jar: &mut CookieJar,
+  ) -> Result<Session> {
+    let key = Key::from(self.context.api_secret_key.as_bytes());
+
     let session_cookie = cookie_jar
       .get(SESSION_KEY)
       .expect("Failed to get current session cookie");
+
+    let id = session_cookie.value();
+
+    let mut current_session = self
+      .context
+      .session
+      .load_session(Uuid::from_str(id)?)
+      .expect(&format!("failed to load session by this id {}", id));
 
     let body = AuthBody {
       client_id: self.context.api_key.clone(),
@@ -85,6 +97,36 @@ impl Client {
       )
       .await?;
 
-    Ok(())
+    let body = request.json::<AuthResponse>().await?;
+
+    let (access_token, scope) = (
+      body.access_token_response.access_token,
+      body.access_token_response.scope,
+    );
+
+    let session_expiration = Utc::now()
+      .checked_add_signed(ChronoDuration::seconds(body.online_accesss_info.expires_in))
+      .expect("failed to add seconds to expiration")
+      .checked_add_signed(ChronoDuration::seconds(1000))
+      .expect("failed to add seconds to expiration");
+
+    current_session.access_token = Some(access_token);
+    current_session.expires = Some(session_expiration);
+    current_session.scope = Some(scope);
+    current_session.online_access_info = Some(body.online_accesss_info);
+
+    let cookie = Cookie::build(SESSION_KEY, current_session.id.to_string())
+      .secure(true)
+      .same_site(cookie::SameSite::Lax)
+      .expires(OffsetDateTime::from_unix_timestamp(
+        session_expiration.timestamp_millis(),
+      )?)
+      .finish();
+
+    cookie_jar.signed_mut(&key).add(cookie);
+
+    self.context.session.store_session(current_session.clone());
+
+    Ok(current_session)
   }
 }
