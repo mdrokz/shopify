3822ec71b1e2b2a4fde6e78e91054ab5f1c15e4f
mdrokz <mohammadmunshi@gmail.com>
2022-05-27 15:08:15 +0530

    add base customer methods

---
+++ shopify/src/customer/mod.rs
@@ -0,0 +1,86 @@
+use crate::client::{Client, Method};
+use crate::result::*;
+
+mod types;
+pub use self::types::*;
+
+impl Client {
+  async fn get_customer_list(&self) -> ShopifyResult<Vec<Customer>> {
+    shopify_wrap! {
+      pub struct Res {
+        customers: Vec<Customer>,
+      }
+    }
+
+    let res: Res = self
+      .request(
+        Method::GET,
+        &format!("/admin/{}/customers.json", self.context.api_version),
+        std::convert::identity,
+      )
+      .await?;
+    Ok(res.into_inner())
+  }
+
+  async fn create_customer(&self, params: &CustomerParams) -> ShopifyResult<Customer> {
+    shopify_wrap! {
+      pub struct Res {
+        customer:Customer,
+      }
+    }
+
+    let res: Res = self
+      .request(
+        Method::POST,
+        &format!("/admin/{}/customers.json", self.context.api_version),
+        |b| b.json(params),
+      )
+      .await?;
+    Ok(res.into_inner())
+  }
+
+  async fn get_customer(&self, id: i64) -> ShopifyResult<Customer> {
+    shopify_wrap! {
+      pub struct Res {
+        customer: Customer,
+      }
+    }
+    let path = format!("/admin/{}/customers/{}.json", self.context.api_version, id);
+    let res: Res = self
+      .request(Method::GET, &path, std::convert::identity)
+      .await?;
+    Ok(res.into_inner())
+  }
+
+  async fn get_customer_orders(&self,id: i64) -> ShopifyResult<()> {
+      Ok(())
+  }
+
+}
+
+
+#[cfg(test)]
+mod tests {
+  #[test]
+  #[ignore]
+  fn test_location_get_list() {
+    use super::LocationApi;
+    let client = ::client::get_test_client();
+    let list = client.get_list().unwrap();
+    println!("{:#?}", list);
+  }
+
+  #[test]
+  #[ignore]
+  fn test_inventory_level_get_list() {
+    use super::{GetInventoryLevelsParams, InventoryLevelApi};
+    let client = ::client::get_test_client();
+    let list = client
+      .get_list(&GetInventoryLevelsParams {
+        inventory_item_ids: Some(vec![2819391175, 5746930631]),
+        ..Default::default()
+      })
+      .unwrap();
+    println!("{:#?}", list);
+  }
+}
