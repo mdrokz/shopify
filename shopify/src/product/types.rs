7097a46d27d3be9285330b1ca0481498e11ddf37
mdrokz <mohammadmunshi@gmail.com>
2022-06-09 18:01:31 +0530

    derive Default & JsonSchema for product

--- shopify/src/product/types.rs
+++ shopify/src/product/types.rs
@@ -1,18 +1,19 @@
+use schemars::JsonSchema;
 use serde_json::Value;
-use crate::types::{DateTime, Utc};
+use crate::types::{Date};
 pub use crate::variant::Variant;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize,Default,JsonSchema)]
 pub struct Product {
   pub id: i64,
   pub title: String,
   pub body_html: String,
   pub vendor: String,
   pub product_type: String,
-  pub created_at: DateTime<Utc>,
+  pub created_at: Date,
   pub handle: String,
-  pub updated_at: Option<DateTime<Utc>>,
-  pub published_at: Option<DateTime<Utc>>,
+  pub updated_at: Option<Date>,
+  pub published_at: Option<Date>,
   pub template_suffix: Value,
   pub tags: String,
   pub published_scope: String,
@@ -22,13 +23,13 @@ pub struct Product {
   pub image: Option<Image>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize,Default,JsonSchema)]
 pub struct Image {
   pub id: i64,
   pub product_id: i64,
   pub position: i64,
   pub created_at: String,
-  pub updated_at: Option<DateTime<Utc>>,
+  pub updated_at: Option<Date>,
   pub alt: Option<String>,
   pub width: i64,
   pub height: i64,
@@ -36,7 +37,7 @@ pub struct Image {
   pub variant_ids: Vec<i64>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize,Default,JsonSchema)]
 pub struct ProductOption {
   pub id: i64,
   pub product_id: i64,
