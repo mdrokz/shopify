d1b11915018d2974558423336b1885aa54c48e98
mdrokz <mohammadmunshi@gmail.com>
2022-06-09 19:46:21 +0530

    change type back to Arg

--- shopify/src/customer/mod.rs
+++ shopify/src/customer/mod.rs
@@ -23,14 +23,14 @@ impl Client {
     Ok(res.into_inner())
   }
 
-  pub async fn create_customer(&self, customer: &Customer) -> ShopifyResult<Customer> {
+  pub async fn create_customer(&self, customer: &CustomerArg) -> ShopifyResult<Customer> {
     shopify_wrap! {
       pub struct Res {
         customer:Customer,
       }
 
       pub struct Body {
-        customer: Customer
+        customer: CustomerArg
       }
     }
 
@@ -93,13 +93,13 @@ impl Client {
     Ok(res.into())
   }
 
-  pub async fn update_customer(&self, customer: &Customer, id: i64) -> ShopifyResult<Customer> {
+  pub async fn update_customer(&self, customer: &CustomerArg, id: i64) -> ShopifyResult<Customer> {
     shopify_wrap! {
       pub struct Res {
         customer: Customer,
       }
       pub struct Body {
-        customer: Customer
+        customer: CustomerArg
       }
     }
     let path = format!(
