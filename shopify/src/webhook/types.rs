5501a556298cbace95489b48abe1ff23aa46edfa
mdrokz <mohammadmunshi@gmail.com>
2022-06-27 17:51:56 +0530

    add type for webhook resource

--- shopify/src/webhook/types.rs
+++ shopify/src/webhook/types.rs
@@ -1,61 +1,94 @@
 use schemars::JsonSchema;
 
-#[derive(Debug, Serialize, Deserialize,Clone,JsonSchema)]
+#[derive(Debug, Serialize, Deserialize, Clone, JsonSchema)]
 pub struct CustomerDataRequest {
-    #[serde(rename = "shop_id")]
-    pub shop_id: i64,
+  #[serde(rename = "shop_id")]
+  pub shop_id: i64,
 
-    #[serde(rename = "shop_domain")]
-    pub shop_domain: String,
+  #[serde(rename = "shop_domain")]
+  pub shop_domain: String,
 
-    #[serde(rename = "orders_requested")]
-    pub orders_requested: Vec<i64>,
+  #[serde(rename = "orders_requested")]
+  pub orders_requested: Vec<i64>,
 
-    #[serde(rename = "customer")]
-    pub customer: Customer,
+  #[serde(rename = "customer")]
+  pub customer: Customer,
 
-    #[serde(rename = "data_request")]
-    pub data_request: DataRequest,
+  #[serde(rename = "data_request")]
+  pub data_request: DataRequest,
 }
 
-#[derive(Debug, Serialize, Deserialize,Clone,JsonSchema)]
+#[derive(Debug, Serialize, Deserialize, Clone, JsonSchema)]
 pub struct CustomerDataRedact {
-    #[serde(rename = "shop_id")]
-    pub shop_id: i64,
+  #[serde(rename = "shop_id")]
+  pub shop_id: i64,
 
-    #[serde(rename = "shop_domain")]
-    pub shop_domain: String,
+  #[serde(rename = "shop_domain")]
+  pub shop_domain: String,
 
-    #[serde(rename = "orders_to_redact")]
-    pub orders_to_redact: Vec<i64>,
+  #[serde(rename = "orders_to_redact")]
+  pub orders_to_redact: Vec<i64>,
 
-    #[serde(rename = "customer")]
-    pub customer: Customer,
+  #[serde(rename = "customer")]
+  pub customer: Customer,
 }
 
-#[derive(Debug, Serialize, Deserialize,Clone,JsonSchema)]
+#[derive(Debug, Serialize, Deserialize, Clone, JsonSchema)]
 pub struct ShopRedact {
-    #[serde(rename = "shop_id")]
-    pub shop_id: i64,
+  #[serde(rename = "shop_id")]
+  pub shop_id: i64,
 
-    #[serde(rename = "shop_domain")]
-    pub shop_domain: String,
+  #[serde(rename = "shop_domain")]
+  pub shop_domain: String,
 }
 
-#[derive(Debug, Serialize, Deserialize,Clone,JsonSchema)]
+#[derive(Debug, Serialize, Deserialize, Clone, JsonSchema)]
 pub struct Customer {
-    #[serde(rename = "id")]
-    pub id: Option<i64>,
+  #[serde(rename = "id")]
+  pub id: Option<i64>,
 
-    #[serde(rename = "email")]
-    pub email: String,
+  #[serde(rename = "email")]
+  pub email: String,
 
-    #[serde(rename = "phone")]
-    pub phone: String,
+  #[serde(rename = "phone")]
+  pub phone: String,
 }
 
-#[derive(Debug, Serialize, Deserialize,Clone,JsonSchema)]
+#[derive(Debug, Serialize, Deserialize, Clone, JsonSchema)]
 pub struct DataRequest {
-    #[serde(rename = "id")]
-    pub id: i64,
+  #[serde(rename = "id")]
+  pub id: i64,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, JsonSchema)]
+pub struct Webhook {
+  #[serde(rename = "address")]
+  pub address: String,
+
+  #[serde(rename = "api_version")]
+  pub api_version: String,
+
+  #[serde(rename = "created_at")]
+  pub created_at: String,
+
+  #[serde(rename = "fields")]
+  pub fields: Vec<String>,
+
+  #[serde(rename = "format")]
+  pub format: String,
+
+  #[serde(rename = "id")]
+  pub id: i64,
+
+  #[serde(rename = "metafield_namespaces")]
+  pub metafield_namespaces: Vec<String>,
+
+  #[serde(rename = "private_metafield_namespaces")]
+  pub private_metafield_namespaces: Vec<String>,
+
+  #[serde(rename = "topic")]
+  pub topic: String,
+
+  #[serde(rename = "updated_at")]
+  pub updated_at: String,
 }
