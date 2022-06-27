a36105e47a418e3d3936561fcdfc00178ab6379b
mdrokz <mohammadmunshi@gmail.com>
2022-06-27 17:47:23 +0530

    add types customer/redact & shop/redact webhooks

--- shopify/src/webhook/types.rs
+++ shopify/src/webhook/types.rs
@@ -19,9 +19,33 @@ pub struct CustomerDataRequest {
 }
 
 #[derive(Debug, Serialize, Deserialize,Clone,JsonSchema)]
+pub struct CustomerDataRedact {
+    #[serde(rename = "shop_id")]
+    pub shop_id: i64,
+
+    #[serde(rename = "shop_domain")]
+    pub shop_domain: String,
+
+    #[serde(rename = "orders_to_redact")]
+    pub orders_to_redact: Vec<i64>,
+
+    #[serde(rename = "customer")]
+    pub customer: Customer,
+}
+
+#[derive(Debug, Serialize, Deserialize,Clone,JsonSchema)]
+pub struct ShopRedact {
+    #[serde(rename = "shop_id")]
+    pub shop_id: i64,
+
+    #[serde(rename = "shop_domain")]
+    pub shop_domain: String,
+}
+
+#[derive(Debug, Serialize, Deserialize,Clone,JsonSchema)]
 pub struct Customer {
     #[serde(rename = "id")]
-    pub id: i64,
+    pub id: Option<i64>,
 
     #[serde(rename = "email")]
     pub email: String,
