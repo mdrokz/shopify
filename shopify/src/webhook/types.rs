9719bd5d3ac01c686464c517784baa5d47c04d3c
mdrokz <mohammadmunshi@gmail.com>
2022-06-27 17:43:04 +0530

    add webhook mod for shopify webhooks

---
+++ shopify/src/webhook/types.rs
@@ -0,0 +1,37 @@
+use schemars::JsonSchema;
+
+#[derive(Debug, Serialize, Deserialize,Clone,JsonSchema)]
+pub struct CustomerDataRequest {
+    #[serde(rename = "shop_id")]
+    pub shop_id: i64,
+
+    #[serde(rename = "shop_domain")]
+    pub shop_domain: String,
+
+    #[serde(rename = "orders_requested")]
+    pub orders_requested: Vec<i64>,
+
+    #[serde(rename = "customer")]
+    pub customer: Customer,
+
+    #[serde(rename = "data_request")]
+    pub data_request: DataRequest,
+}
+
+#[derive(Debug, Serialize, Deserialize,Clone,JsonSchema)]
+pub struct Customer {
+    #[serde(rename = "id")]
+    pub id: i64,
+
+    #[serde(rename = "email")]
+    pub email: String,
+
+    #[serde(rename = "phone")]
+    pub phone: String,
+}
+
+#[derive(Debug, Serialize, Deserialize,Clone,JsonSchema)]
+pub struct DataRequest {
+    #[serde(rename = "id")]
+    pub id: i64,
+}
